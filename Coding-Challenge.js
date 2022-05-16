ff0ec771a3114a81fe78b4839799f1e479b5e7c7
Fahim015 <fahimjaved1245@gmail.com>
2022-05-16 14:17:11 +0500

    add more data about lecture in (coding-challenge and script files

--- Coding-Challenge.js
+++ Coding-Challenge.js
@@ -1,6 +1,6 @@
 
 
-// /*Coding Challenge #1 
+// Coding Challenge #1 
 
 // Mark and John are trying to compare their BMI (Body Mass Index), 
 // which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height)
@@ -27,3 +27,22 @@ const markHigherBMI = BMIMark > BMIJohn;
 console.log(BMIMark, BMIJohn, markHigherBMI);*/
 
 
+// Coding Challenge #2 
+
+
+// Use the BMI example from Challenge #1, and the code you already wrote, and improve it. 
+// Your tasks: 
+// 1. Print a nice output to the console, saying who has the higher BMI.
+//    The message is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!" 
+// 2. Use a template literal to include the BMI values in the outputs.
+//    Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
+
+/*const massMark =75;
+const heightMark =1.69;
+const massJohn =1.95;
+const heightJohn =92;
+const BMIMark = massMark / heightMark ** 2; 
+const BMIJohn = massJohn / heightJohn ** 2;
+
+
+console.log(`'Mark's BMI' ${BMIMark} 'is higher than John's' ${BMIJohn} `);*/
--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-let firstName = "Fahim";
+/*let firstName = "Fahim";
 
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
-console.log(15 + 30 - 10);
+console.log(15 + 30 - 10);*/
+
+
+
+
+// String and template literals
+
+
+/*const firstName = 'Ahmad';
+const currentStatus = 'Student';
+const birthYear = 1999;
+const year = 2022;
+console.log("I'm " + firstName + " a " + (year - birthYear) + " Years old " + currentStatus);
+
+// we use (~) backtick in witch we use $(variable); instance of + to add more veriable 
+// it was more easy to mange space between variable
+
+const ahmadNew = `I'm ${firstName}, a ${year - birthYear} years old ${currentStatus}`;
+console.log(ahmadNew);
+
+console.log(`multiple 
+line
+string`)*/
+
 

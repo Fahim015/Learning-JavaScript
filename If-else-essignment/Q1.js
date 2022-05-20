885e2a1e4999f2bccfcb1243c26d8cf58fa26cf6
M.Fahim Javed <105284394+Fahim015@users.noreply.github.com>
2022-05-20 08:10:21 +0500

    adding Q1 Solution

---
+++ If-else-essignment/Q1.js
@@ -0,0 +1,14 @@
+
+/* Write a program that reads user age and prints
+“CHILD” if age <18,
+“ADULT” if 18<=age<35 and
+“SENIOR” if age >=35?*/ 
+
+const age = Number (prompt(`Enter Your Age`)); 
+if(age<18) {
+        console.log('CHILD');
+} else if(18<=age<35) {
+    console.log('ADULT');
+} else if(age>=35) {
+    console.log('SENIOR');
+}

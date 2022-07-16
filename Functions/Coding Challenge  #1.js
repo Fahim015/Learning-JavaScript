777fec5a30aac30cd501dbbcd0f8d0aec74cacf6
Fahim015 <fahimjaved1245@gmail.com>
2022-07-16 21:36:42 +0500

    rename file

--- Functions/Coding Challenge  #1.js
+++ Functions/Coding Challenge  #1.js
@@ -3,7 +3,7 @@
 //Coding Challenge #1
 
 
-/*const calcAverage = (a,b,c) => (a+b+c) /3;
+const calcAverage = (a,b,c) => (a+b+c) /3;
 
 console.log(calcAverage(4,6,8));
 const scoreDolphins = calcAverage(44,23,71);
@@ -18,4 +18,21 @@ if(avgDolphins >= 2 * avgKoalas){
     console.log(`Match Draw No Team Win`);
 }
 }
-checkWinner(scoreDolphins,scoreKoalas);*/
+checkWinner(scoreDolphins,scoreKoalas);
+
+
+/////////////////////////////////////////////////////////////////////
+
+//coding Challenge #2
+
+const calcTip = bill => 50>=bill && bill<=300 ? bill*015 : bill*0.2; 
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])];
+const total = [bills[0]+tips[0],bills[1]+tips[1],bills[2]+tips[2]];
+
+console.log(bills,tips,total);
+
+
+

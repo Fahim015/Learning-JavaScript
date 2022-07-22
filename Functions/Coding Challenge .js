c37743371565f64215d30e4f73c794a3cd963bee
Fahim015 <fahimjaved1245@gmail.com>
2022-07-22 17:29:38 +0500

    Adding Codding Challenge# 2,3,4in Function Folder

--- Functions/Coding Challenge .js
+++ Functions/Coding Challenge .js
@@ -1,7 +1,7 @@
 
 //////////////////////////////////////////////////////////////////////
 //Coding Challenge #1
-
+/*
 
 const calcAverage = (a,b,c) => (a+b+c) /3;
 
@@ -19,3 +19,79 @@ if(avgDolphins >= 2 * avgKoalas){
 }
 }
 checkWinner(scoreDolphins,scoreKoalas);
+*/
+
+/////////////////////////////////////////////////////////////////////
+//coding Challenge #2
+/*
+const calcTip = bill => 50>=bill && bill<=300 ? bill*015 : bill*0.2; 
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [calcTip(bills[0]),calcTip(bills[1]),calcTip(bills[2])];
+const total = [bills[0]+tips[0],bills[1]+tips[1],bills[2]+tips[2]];
+
+console.log(bills,tips,total);
+
+*/
+/////////////////////////////////////////////////////////////////////
+//Codding Challenge #3
+/*
+const mark = {
+    fullName: 'Mark Miller',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function(){
+       this.bmi= this.mass/this.height **2;
+    return this.bmi;
+    }
+
+    
+};
+
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function(){
+        this.bmi= this.mass/this.height **2;
+        return this.bmi;
+    }
+    
+};
+
+mark.calcBMI();
+john.calcBMI();
+console.log(mark.bmi, john.bmi);
+if(mark.bmi > john.bmi){
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher then 
+    ${john.fullName}'s BMI (${john.bmi})
+    `)
+}else if (john.bmi > mark.bmi){
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher then 
+    ${mark.fullName}'s BMI (${mark.bmi})
+    `)
+}
+*/
+/////////////////////////////////////////////////////////////////////
+
+//Coding Challenge # 4
+/*
+const calcTip = function(bill){
+    return bill>=50 && bill<=300? bill*0.15: bill *0.2;
+}
+
+const bills = [22,292,176,440,37,105,10,1100,86,52];
+const tips = [];
+const totals = [];
+
+for(let i = 0; i < bills.length; i++){
+const tip = calcTip(bills[i]);
+tips.push(tip);
+totals.push(tip+bills[i]);
+}
+console.log(`Bills is: [${bills}] and 
+Tips is: [${tips}] and 
+Total is: [${totals}]`);
+*/
+//////////////////////////////////////////////////////////////////////
